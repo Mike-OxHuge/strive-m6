d5409f6f9a77e44a6495a16f51159c0f1f007151
Mike-OxHuge <mikelitoris34@icloud.com>
2021-07-13 18:49:40 +0200

    full CRUD w/ relations

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -35,54 +35,46 @@ router
       next(error);
     }
   });
-// "category": "ARTICLE CATEGORY",
-// "title": "ARTICLE TITLE",
-// "cover":"ARTICLE COVER (IMAGE LINK)",
-// "read_time_value": 2,
-// "read_time_unit": "minute"
-// "author":"AUTHOR FOREIGN KEY",
-// "content": "HTML",
 
-// {
-//     "category": "ARTICLE CATEGORY",
-//     "title": "ARTICLE TITLE",
-//     "cover":"ARTICLE COVER (IMAGE LINK)",
-//     "read_time_value": 2,
-//     "read_time_unit": "minute"
-//     "author_id": 7,
-//     "content": "HTML"
-// }
-// router
-//   .route("/:postId")
-//   .get(async (_req, res, next) => {
-//     try {
-//       const query = ``;
-//       const result = await db.query(query);
-//       res.send(result.rows);
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   })
-//   .put(async (_req, res, next) => {
-//     try {
-//       const query = ``;
-//       const result = await db.query(query);
-//       res.send(result.rows);
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   })
-//   .delete(async (_req, res, next) => {
-//     try {
-//       const query = ``;
-//       const result = await db.query(query);
-//       res.send(result.rows);
-//     } catch (error) {
-//       console.log(error);
-//       next(error);
-//     }
-//   });
+router
+  .route("/:postId")
+  .get(async (_req, res, next) => {
+    try {
+      const query = `SELECT * FROM posts WHERE post_id=${_req.params.postId}`;
+      const result = await db.query(query);
+      res.send(result.rows);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .put(async (_req, res, next) => {
+    try {
+      const query = `UPDATE posts SET
+                    category='${_req.body.category}', 
+                    title='${_req.body.title}', 
+                    cover='${_req.body.cover}', 
+                    read_time_value=${parseInt(_req.body.read_time_value)}, 
+                    read_time_unit='${_req.body.read_time_unit}', 
+                    content='${_req.body.content}'
+                        WHERE post_id=${_req.params.postId}
+                            RETURNING*`;
+      const result = await db.query(query);
+      res.send(result.rows);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .delete(async (_req, res, next) => {
+    try {
+      const query = `DELETE FROM posts WHERE post_id=${_req.params.postId} RETURNING* `;
+      const result = await db.query(query);
+      res.send(result.rows);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
 
 export default router;
